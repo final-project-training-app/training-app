782df0e707f04902b3abd09448630eefb92fb598
venujan nagendirakumar <venujan25@outlook.com>
2026-05-06 11:25:04 +0200

    Implement Update user profile

--- backend/src/main/java/com/example/trainingapp/controller/UserController.java
+++ backend/src/main/java/com/example/trainingapp/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,10 @@ public class UserController {
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok().body(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUserPreferences(@PathVariable Long id, @RequestBody User userRequest) {
+        return ResponseEntity.ok().body(userService.updateUserPreferences(id, userRequest));
     }
 }
