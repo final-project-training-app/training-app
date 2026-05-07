66ec35824e61eb5ce1d1e4fce40c4f6c26087982
Tehreem Asif <tehreem.asif63@gmail.com>
2026-05-07 12:01:01 +0200

    Add basic settings modal sheet

---
+++ frontend/src/features/HomePage/components/SettingsModalSheet.tsx
@@ -0,0 +1,11 @@
+export default function SettingsModalSheet() {
+  return (
+  <>
+    <div className="main">
+       <h1>Inställningar</h1>
+       <section><h2>Intensitet</h2></section>
+       <section><h2>Kontext</h2></section> 
+    </div>
+  </>
+  );
+}
