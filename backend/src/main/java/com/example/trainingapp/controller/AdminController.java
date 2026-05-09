725d1b0ada32e3509d9afce0dbada7f28868c49e
Tehreem Asif <tehreem.asif63@gmail.com>
2026-05-09 16:35:11 +0200

    Add basic endpoint for admin only

--- backend/src/main/java/com/example/trainingapp/controller/AdminController.java
+++ backend/src/main/java/com/example/trainingapp/controller/AdminController.java
@@ -1,7 +1,11 @@
 package com.example.trainingapp.controller;
 
 import com.example.trainingapp.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,22 @@ public class AdminController {
 
     public AdminController(UserService service) {
         this.service = service;
+    }
+    private String getClerkId(Authentication authentication) {
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        return jwt.getSubject();
+    }
+
+    @GetMapping
+    public ResponseEntity<String> adminPage(Authentication authentication) {
+
+        String clerkId = getClerkId(authentication);
+
+        if (!service.isAdmin(clerkId)) {
+            return ResponseEntity.status(403).body("Forbidden");
+        }
+
+        return ResponseEntity.ok("Admin page");
     }
 
 }
